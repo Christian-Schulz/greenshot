b95f59e564a2475d61706555664b4000aa0ea21e
Christian Schulz <chris@schocktools.de>
2020-01-17 22:13:16 +0100

    Wait for SystemTextJson

--- src/Greenshot.Addon.Redmine/Api/RedmineConnector.cs
+++ src/Greenshot.Addon.Redmine/Api/RedmineConnector.cs
@@ -1,37 +1,34 @@
 ﻿// Greenshot - a free and open source screenshot tool
 // Copyright (C) 2007-2020 Thomas Braun, Jens Klingen, Robin Krom
-// 
+//
 // For more information see: http://getgreenshot.org/
 // The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 1 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-using Dapplo.HttpExtensions;
-using Dapplo.HttpExtensions.JsonNet;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Dapplo.HttpExtensions;
 using Dapplo.HttpExtensions.Factory;
+//TODO use System.Text.Json (Dapplo.HttpExtensions.SystemTextJson)
+//using System.Text.Json;
 using Dapplo.HttpExtensions.JsonNet;
 using Dapplo.Log;
 using Greenshot.Addon.Redmine.Api.Types;
 using Greenshot.Addon.Redmine.Configuration;
 using Greenshot.Addons.Core;
-using System;
-using System.Threading.Tasks;
-using System.Net;
-using System.Threading;
-using Newtonsoft.Json;
 
 namespace Greenshot.Addon.Redmine.Api
 {
@@ -53,12 +50,13 @@ namespace Greenshot.Addon.Redmine.Api
                 ThrowOnError = false, //TODO: überlege, ob wirklich notwendig
                 HttpSettings = httpConfiguration,
                 JsonSerializer = new JsonNetJsonSerializer(),
+                //TODO use System.Text.Json (Dapplo.HttpExtensions.SystemTextJson)
+                //JsonSerializer = new SystemTextJsonSerializer(),
                 OnHttpClientCreated = httpClient =>
                 {
                     httpClient.AddDefaultRequestHeader("X-Redmine-API-Key", _redmineConfiguration.APIToken);
-                    httpClient.DefaultRequestHeaders.ExpectContinue = false;                    
+                    httpClient.DefaultRequestHeaders.ExpectContinue = false;
                 }
-                
             };
         }
 
@@ -89,14 +87,11 @@ namespace Greenshot.Addon.Redmine.Api
                 //TODO: CurrentUser durch dynamic ersetzen und local User-Objekt daraus machen
 
                 return await response.GetAsAsync<CurrentUser>(token);
-
             }
             catch (Exception ex)
             {
-
                 throw;
             }
-
         }
     }
 }
