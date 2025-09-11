1c5efac11d7878857afa662a6d646537781ebb5b
Christian Schulz <Chris@schocktools.de>
2025-09-11 23:08:20 +0200

    Refactor: Replace WM_COPYDATA with Named Pipes for IPC
    Replace BinaryFormatter with MessagePack for IPC-Data

---
+++ src/Greenshot/Ipc/IpcHelper.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2025 Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: https://getgreenshot.org/
+ * The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using log4net;
+using MessagePack;
+
+namespace Greenshot.Ipc;
+
+internal static class IpcHelper
+{
+    private static readonly ILog Log = LogManager.GetLogger(typeof(IpcHelper));
+
+    public const string PipePrefix = "Greenshot_Pipe_ID";
+    public const string TargetProcessName = "Greenshot";
+
+    public const int MaxAllowedReceivedBytes = 1 * 1024 * 1024; // 1MB
+
+    public static string GetPipeName(int processId) => PipePrefix + processId;
+
+    public static AppCommands Deserialize(byte[] rawData)
+    {
+        AppCommands appCommands;
+        try
+        {
+            var msg = MessagePackSerializer.Deserialize<IpcMessage>(rawData);
+            appCommands = new AppCommands(msg?.Commands ?? []);
+        }
+        catch (Exception e)
+        {
+            Log.ErrorFormat("Could not deserialize data: {0}", e.Message);
+            throw;
+        }
+        return appCommands;
+    }
+
+    public static byte[] Serialize(AppCommands appCommands)
+    {
+        byte[] bytes;
+        try
+        {
+            var dto = new IpcMessage { Commands = appCommands.Commands }; 
+            bytes = MessagePackSerializer.Serialize(dto);
+        }
+        catch (Exception e)
+        {
+            Log.ErrorFormat("Could not serialize appCommands: {0}", e.Message);
+            throw;
+        }
+        return bytes;
+    }
+
+}
---
+++ src/Greenshot/Ipc/IpcSender.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Greenshot - a free and open source screenshot tool
+ * Copyright (C) 2007-2025 Thomas Braun, Jens Klingen, Robin Krom
+ * 
+ * For more information see: https://getgreenshot.org/
+ * The Greenshot project is hosted on GitHub https://github.com/greenshot/greenshot
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using log4net;
+
+namespace Greenshot.Ipc;
+
+internal static class IpcSender
+{
+    private static readonly ILog Log = LogManager.GetLogger(typeof(IpcListener));
+
+    public static void Send(AppCommands appCommands)
+    {
+        Broadcast(appCommands);
+    }
+
+    private static void Broadcast(AppCommands commands)
+    {
+        var datatoSend = IpcHelper.Serialize(commands);
+        var currentPid = Process.GetCurrentProcess().Id;
+
+        // try to send to all processes with the same name
+        foreach (var process in Process.GetProcessesByName(IpcHelper.TargetProcessName))
+        {
+            try
+            {
+                if (process.Id == currentPid)
+                {
+                    continue; // do not send to current process itself
+                }
+                var pipeName = IpcHelper.GetPipeName(process.Id);
+                Log.DebugFormat("Sending AppCommands {0} to Pipe {1}", commands, pipeName);
+
+                using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+                client.Connect(100); // max. warten 100ms
+                using var writer = new BinaryWriter(client);
+                writer.Write(datatoSend);
+            }
+            catch (TimeoutException)
+            {
+                // Instanz läuft zwar, aber pipe hört (noch) nicht zu
+            }
+            catch (IOException)
+            {
+                // Pipe gerade nicht erreichbar
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Broadcast-Fehler: " + ex.Message);
+            }
+        }
+    }
+
+}
+
